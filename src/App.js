b61e0723efb70cc17b4edbb9d4f731626815aefe
creditvilleNg <chioma.chukelu@creditville.ng>
2022-12-19 19:31:45 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import logo from './images/Group.png';
+import logos from './images/Group1915.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p>hello word</p>
-      </header>
+      {/* <header className="App-header"> */}
+       <div children="nav">
+        <img src={logo}/>
+       </div>
+       <div children="nav">
+        <img src={logos}/>
+       </div>
+       <div>
+
+       </div>
+      {/* </header> */}
     </div>
   );
 }
